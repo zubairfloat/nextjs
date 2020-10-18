015aff819d940212351e540a8cd96d8b861023e8
Zubair Rizwan <zubairfloat@gmail.com>
2020-10-19 03:19:42 +0500

    carousel

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import Navbar from '../Components/navar';
 import styles from '../styles/Home.module.css';
 import SideMenue from '../Components/sideMenu';
+import Carousel from '../Components/carousel';
 
 export default function Home() {
   return (
@@ -37,77 +38,8 @@ export default function Home() {
           <div className='col-lg-3'>
             <SideMenue />
           </div>
-
           <div className='col-lg-9'>
-            <div
-              id='carouselExampleIndicators'
-              className='carousel slide my-4'
-              data-ride='carousel'
-            >
-              <ol className='carousel-indicators'>
-                <li
-                  data-target='#carouselExampleIndicators'
-                  data-slide-to='0'
-                  className='active'
-                ></li>
-                <li
-                  data-target='#carouselExampleIndicators'
-                  data-slide-to='1'
-                ></li>
-                <li
-                  data-target='#carouselExampleIndicators'
-                  data-slide-to='2'
-                ></li>
-              </ol>
-              <div className='carousel-inner' role='listbox'>
-                <div className='carousel-item active'>
-                  <img
-                    className='d-block img-fluid'
-                    src='http://placehold.it/900x350'
-                    alt='First slide'
-                  />
-                </div>
-                <div className='carousel-item'>
-                  <img
-                    className='d-block img-fluid'
-                    src='http://placehold.it/900x350'
-                    alt='Second slide'
-                  />
-                </div>
-                <div className='carousel-item'>
-                  <img
-                    className='d-block img-fluid'
-                    src='http://placehold.it/900x350'
-                    alt='Third slide'
-                  />
-                </div>
-              </div>
-              <a
-                className='carousel-control-prev'
-                href='#carouselExampleIndicators'
-                role='button'
-                data-slide='prev'
-              >
-                <span
-                  className='carousel-control-prev-icon'
-                  aria-hidden='true'
-                ></span>
-                <span className='sr-only'>Previous</span>
-              </a>
-              <a
-                className='carousel-control-next'
-                href='#carouselExampleIndicators'
-                role='button'
-                data-slide='next'
-              >
-                <span
-                  className='carousel-control-next-icon'
-                  aria-hidden='true'
-                ></span>
-                <span className='sr-only'>Next</span>
-              </a>
-            </div>
-
+            <Carousel />
             <div className='row'>
               <div className='col-lg-4 col-md-6 mb-4'>
                 <div className='card h-100'>
